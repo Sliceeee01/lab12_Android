b8f177ff8c595f23e5daebddd371ff0a1dc94ed2
Sliceeee01 <ivan_hih@icloud.com>
2026-05-23 18:20:49 +0300

    Step 5: Basic model and file

---
+++ app/src/main/java/com/example/grade_clicker/model/Grade.kt
@@ -0,0 +1,10 @@
+package com.example.grade_clicker.model
+
+import androidx.annotation.DrawableRes
+
+data class Grade(
+    @DrawableRes val imageId: Int,
+    val pointsPerClick: Int,
+    val threshold: Int,
+
+)
